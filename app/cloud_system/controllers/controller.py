910f9ccf9b71f221cef241957a3a16036423ca13
Ziwei Gu <zg48@cornell.edu>
2021-05-08 14:26:12 -0400

    Initialize dashboard

--- app/cloud_system/controllers/controller.py
+++ app/cloud_system/controllers/controller.py
@@ -1,20 +1,22 @@
 from . import *  
 from app.cloud_system.models.helpers import *
 from app.cloud_system.models.helpers import NumpyEncoder as NumpyEncoder
+import random
 
 project_name = "CS 5412 Cloud Computing Final Project"
 net_id = "Gia Yao (yy667), Tianxing Jiang (tj258), Ziwei Gu (zg48)"
 
 @cloud_system.route('/', methods=['GET'])
 def search():
-	query = request.args.get('search')
-	if not query:
-		data = []
-		output_message = ''
-	else:
-		output_message = "Your search: " + query
-		data = range(5)
-	return render_template('search.html', name=project_name, netid=net_id, output_message=output_message, data=data)
+	# query = request.args.get('search')
+	# if not query:
+	# 	data = []
+	# 	output_message = ''
+	# else:
+	# 	output_message = "Your search: " + query
+	# 	data = range(5)
+	data = [{'id': i, 'col_1': random.randint(1, 100), 'col_2': random.randint(1, 100), 'col_3': random.randint(1, 100), 'col_4': random.randint(1, 100)} for i in range(1, 20)]
+	return render_template('search.html', data=data)
 
 
 
