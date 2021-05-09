65385a0286e963192196429aa54cf11e1d504bd8
Tianxing Jiang <tj258@cornell.edu>
2021-05-09 02:08:43 +0000

    Update connection string

--- iot/simulated_sensor.py
+++ iot/simulated_sensor.py
@@ -1,10 +1,11 @@
 import random
 import time
 import csv
+from datetime import datetime
 
 from azure.iot.device import IoTHubDeviceClient, Message
 
-CONNECTION_STRING = "HostName=final-project-iot-hub.azure-devices.net;DeviceId=PythonSensor1;SharedAccessKey=C2Ze1iwVqBcHfrrsv0NkxpoVlk6EZqE2yAaqOoluKQY="
+CONNECTION_STRING = "HostName=final-project-iot.azure-devices.net;DeviceId=PythonSensor1;SharedAccessKey=egKgXyL3wFDD5ZU8v1e7RUtJ3FE6IGG9T+18qr3PDuY="
 CSV_FILE_PATH = "../../SB_May20_Nov20.csv"
 
 def iothub_client_init():
@@ -20,7 +21,9 @@ def iothub_client_telemetry_sample_run():
             with open(CSV_FILE_PATH, 'r') as file:
                 reader = csv.DictReader(file)
                 for row in reader:
-                    message = Message(str(dict(row)))
+                    msg_dict = dict(row)
+                    msg_dict['DateTime'] = datetime.now().isoformat() # current time
+                    message = Message(str(msg_dict))
                     print(f"Sending message: {message}")
                     client.send_message(message)
                     time.sleep(10)
