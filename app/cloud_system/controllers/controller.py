a1bcb90e5806aa2f8c988791b435532910147454
Ziwei Gu <zg48@cornell.edu>
2021-05-10 23:34:08 -0400

    Connect excel data

--- app/cloud_system/controllers/controller.py
+++ app/cloud_system/controllers/controller.py
@@ -1,7 +1,8 @@
 from . import *  
 from app.cloud_system.models.helpers import *
 from app.cloud_system.models.helpers import NumpyEncoder as NumpyEncoder
-import random
+# import random
+import json
 
 project_name = "CS 5412 Cloud Computing Final Project"
 net_id = "Gia Yao (yy667), Tianxing Jiang (tj258), Ziwei Gu (zg48)"
@@ -15,7 +16,9 @@ def search():
 	# else:
 	# 	output_message = "Your search: " + query
 	# 	data = range(5)
-	data = [{'id': i, 'col_1': random.randint(1, 100), 'col_2': random.randint(1, 100), 'col_3': random.randint(1, 100), 'col_4': random.randint(1, 100)} for i in range(1, 20)]
+	with open('example.json') as f:
+		data = json.load(f)
+	# data = [{'id': i, 'col_1': random.randint(1, 100), 'col_2': random.randint(1, 100), 'col_3': random.randint(1, 100), 'col_4': random.randint(1, 100)} for i in range(1, 20)]
 	return render_template('search.html', data=data)
 
 
