864360cf422b37d1bf54a22df362764f858a7d20
Tianxing Jiang <tj258@cornell.edu>
2021-05-17 02:16:50 -0400

    working

--- app/__init__.py
+++ app/__init__.py
@@ -7,6 +7,7 @@ import os
 from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_socketio import SocketIO
+from flask_cors import CORS
 
 # Configure app
 socketio = SocketIO()
@@ -22,6 +23,8 @@ from app.accounts import accounts as accounts
 app.register_blueprint(accounts)
 from app.cloud_system import cloud_system as cloud_system
 app.register_blueprint(cloud_system)
+
+CORS(app)
 
 # Initialize app w/SocketIO
 socketio.init_app(app)
